9f1107e0def691a7e8cad2a035c0d87741d2dca2
Rishabh Jain <rishabh101.99@gmail.com>
2019-01-04 22:57:36 -0800

    plays a full game if player places ideal first move

--- HardCoded_Unbeatable_Tic-Tac-Toe_AI.py
+++ HardCoded_Unbeatable_Tic-Tac-Toe_AI.py
@@ -1,4 +1,9 @@
+import random
+import os
+
 def displayBoard(board):
+    clear = lambda: os.system('cls')
+#    clear()
     for x in range(len(board)):
         line = ""
         for y in range(len(board[0])):
@@ -38,19 +43,106 @@ def checkBoard(board):
     else:
         return "continue game"
 
+def getCellByNum(board, num):
+    num -= 1
+    return board[int(num / 3)][num % 3]
+
+def setCellByNum(board, num, side):
+    num -= 1
+    board[int(num / 3)][num % 3] = side
+
 def userTurn(board):
-    num = int(input("Where would you like to move? ")) - 1
-    board[int(num / 3)][num % 3] = "X"
+    num = int(input("Where would you like to move? "))
+    setCellByNum(board, num, "X")
     displayBoard(board)
 
-def aiTurn(board):
-    return ""
+def placeCorner(board, place):
+    if (place == 0):
+        board[0][0] = "O"
+    elif(place == 1):
+        board[0][2] = "O"
+    elif(place == 2):
+        board[2][2] = "O"
+    elif(place == 3):
+        board[2][0] = "O"
+
+def getNumBetweenCorners(a, b):
+    if (a + b == 1):        # 0 - 1
+        return 2;
+    elif(a + b == 3 and abs(a - b) == 1):       # 1 - 2
+        return 6;
+    elif(a + b == 5 and abs(a - b) == 1):       # 2 - 3
+        return 8;
+    elif(abs(a - b) == 3):    # 3 - 0
+        return 4;
+    elif(a + b == 4):
+        return 5
+    elif(a + b == 2):
+        return 5
+
+def cornerToRegNum(num):
+    if(num == 0):
+        return 1
+    elif(num == 1):
+        return 3
+    elif(num == 2):
+        return 9
+    elif(num == 3):
+        return 7
+
+def aiTurn(board, move, prevList):
+    prev = prevList[len(prevList) - 1]
+    if (move == 0):
+        rand = random.randint(0,3)
+        prev = rand;
+        placeCorner(board, prev)
+    elif(move == 1):
+        if (board[1][1] == "X"):
+            prev = int((prev + 2) % 4)
+            placeCorner(board, prev)
+        else:
+            rand = 2 * random.randint(0, 1);
+            prev = int((prev + 3 + rand) % 4)
+            placeCorner(board, prev)
+    elif(move == 2):
+        if (getCellByNum(board, getNumBetweenCorners(prevList[1], prevList[2])) != "X"):
+            setCellByNum(board, getNumBetweenCorners(prevList[1], prevList[2]), "O")
+        else:
+            a = len(prevList) - 2
+            b = prev
+            small = min(a, b)
+            large = max(a, b)
+            if (getCellByNum(board, cornerToRegNum(int((large + 1) % 4))) == "X"):
+                prev = int((small + 3) % 4)
+            elif(getCellByNum(board, cornerToRegNum(int((small + 3) % 4))) == "X"):
+                prev = int((large + 1) % 4)
+            else:
+                rand = random.randint(0, 1);
+                prev = int((large + 1 + prev) % 4)
+            print(prev)
+            placeCorner(board, prev)
+    elif(move == 3):
+        if (getCellByNum(board, getNumBetweenCorners(prevList[3], prevList[1])) == " "):
+            prev = getNumBetweenCorners(prevList[3], prevList[1])
+        elif(getCellByNum(board, getNumBetweenCorners(prevList[3], prevList[2])) == " "):
+            prev = getNumBetweenCorners(prevList[3], prevList[2])
+        setCellByNum(board, prev, "O")
+    else:   
+        for i in range(9):
+            if (getCellByNum(board, i + 1)):
+                prev = i + 1
+        setCellByNum(board, prev, "O")
+    displayBoard(board)
+    return prev
 
 def gamePlay(board):
-    userTurn(board);
+    move = 0
+    prev = [-1]
+    prev.append(aiTurn(board, move, prev))
     while(checkBoard(board) == "continue game"):
-        aiTurn(board)
+        move += 1
         userTurn(board);
+        prev.append(aiTurn(board, move, prev))
     print(checkBoard(board))
 
 board = [ [" ", " ", " "], [" ", " ", " "], [" ", " ", " "] ]
